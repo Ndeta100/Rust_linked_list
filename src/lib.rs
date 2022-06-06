06742152f339de46c308a2e82bd836fc932daba5
Ndeta Innocent <you@example.com>
2022-06-07 01:57:06 +0300

    tryingout structs and enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
-enum Node {
-    Empty,
-    NonEmpty(u32),
+struct Node {
+    element: u32,
+    next: Link,
 }
 
+struct LinkedList {
+    head: Link,
+}
+enum Link {
+    Empty,
+    NonEmpty(Box<Node>),
+}
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let list = Link::NonEmpty(Box::new(Node {
+            element: 103,
+            next: Link::Empty,
+        }));
     }
 }
